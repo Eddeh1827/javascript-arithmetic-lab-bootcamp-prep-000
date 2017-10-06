ba8a59099e6549214ae0af4363b20ed0128a29fa
Eddie Bickham <bickhame@gmail.com>
2017-10-06 06:12:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,10 @@ function divide(a, b) {
   return a / b;
 };
 function inc(n) {
-  var n = 540;
-  return n+=;
+  return n;
 };
 function dec(n) {
-  var n = 91;
-  return n-=;
+  return n;
 };
 function makeInt(n) {
   var n = "2"
